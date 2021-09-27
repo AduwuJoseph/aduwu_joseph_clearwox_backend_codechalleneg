741e9cdf6be32f2994ae72df3a3b9d27886e6dd5
Joseph Aduwu <joseph@exarotech.com>
2021-09-27 14:02:44 +0100

    question 2 solution

---
+++ ClearwoxCodeChallenge.API/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClearwoxCodeChallenge.API.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        [StringLength(100)]
+        [Required]
+        public string FirstName { get; set; }
+        [StringLength(100)]
+        [Required]
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string Address { get; set; }
+
+    }
+}
