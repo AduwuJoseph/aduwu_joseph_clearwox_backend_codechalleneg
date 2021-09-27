0f54d5f78d18310c3453415dfcffb41033eee17c
Joseph Aduwu <joseph@exarotech.com>
2021-09-27 17:14:47 +0100

    question 7 solution

--- ClearwoxCodeChallenge.Consol/Program.cs
+++ ClearwoxCodeChallenge.Consol/Program.cs
@@ -9,7 +9,17 @@ namespace ClearwoxCodeChallenge.Consol
             Console.WriteLine("Clearwox Code Challenge Question 6!");
 
             string lines = string.Join(Environment.NewLine, QuestionClass.NameList);
+
             Console.WriteLine(lines);
+
+            Console.WriteLine(QuestionClass.TESTModule(1));
+            Console.WriteLine(QuestionClass.TESTModule(4));
+            Console.WriteLine(QuestionClass.TESTModule(6));
+            Console.WriteLine(QuestionClass.TESTModule(1.0f));
+            Console.WriteLine(QuestionClass.TESTModule(2.0f));
+            Console.WriteLine(QuestionClass.TESTModule(5.0f));
+            Console.WriteLine(QuestionClass.TESTModule("samuel"));
+            Console.WriteLine(QuestionClass.TESTModule("#"));
         }
     }
 }
--- ClearwoxCodeChallenge.Consol/QuestionClass.cs
+++ ClearwoxCodeChallenge.Consol/QuestionClass.cs
@@ -14,5 +14,35 @@ namespace ClearwoxCodeChallenge.Consol
             "Jeffrey",
             "John"
         };
+
+        public static dynamic TESTModule(dynamic value)
+        {
+            dynamic result;
+            switch (value)
+            {
+                case < 1:
+                    result = "throw proper exception";
+                    break;
+                case <= 4 :
+                    result = value * 2;
+                    break;
+                case > 4:
+                    result = value * 3;
+                    break;
+                case 1.0f:
+                    result = 3.0f;
+                    break;
+                case 3.0f:
+                    result = 3.0f;
+                    break;
+                default:
+                    string str = Convert.ToString(value);
+                    str = str.ToUpper();
+                    result = str;
+                    break;
+
+            }
+            return result;
+        }
     }
 }
